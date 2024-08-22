6bf49bd5ce8aa065bc9924464ce79b80f669e5e6
Saumya Rai <saumyarai@RGGYSMAC-48.local>
2024-08-23 00:05:59 +0530

    comments removed

--- src/store.js
+++ src/store.js
@@ -1,31 +1,31 @@
-// import { createStore } from 'redux';
-// import { combineReducers } from 'redux';
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
 
-// const initialState = {
-//   favorites: [],
-// };
+const initialState = {
+  favorites: [],
+};
 
-// function favoritesReducer(state = initialState, action) {
-//   switch (action.type) {
-//     case 'ADD_FAVORITE':
-//       return {
-//         ...state,
-//         favorites: [...state.favorites, action.payload],
-//       };
-//     case 'REMOVE_FAVORITE':
-//       return {
-//         ...state,
-//         favorites: state.favorites.filter(movie => movie.imdbID !== action.payload),
-//       };
-//     default:
-//       return state;
-//   }
-// }
+function favoritesReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_FAVORITE':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload],
+      };
+    case 'REMOVE_FAVORITE':
+      return {
+        ...state,
+        favorites: state.favorites.filter(movie => movie.imdbID !== action.payload),
+      };
+    default:
+      return state;
+  }
+}
 
-// const rootReducer = combineReducers({
-//   favorites: favoritesReducer,
-// });
+const rootReducer = combineReducers({
+  favorites: favoritesReducer,
+});
 
-// const store = createStore(rootReducer);
+const store = createStore(rootReducer);
 
-// export default store;
+export default store;
